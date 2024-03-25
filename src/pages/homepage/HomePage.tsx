ad46aa75111ee8eb619227f77bde17d3a9c3fe80
atakan ihtiyar <ataberk97@hotmail.com>
2024-03-25 18:36:10 +0300

    add hero to homepage

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,10 +1,27 @@
 import "./styles/HomePage.css"
 import PlanetView from '../../components/planet_view/PlanetView';
+import Text, { TextTemplates } from "../../components/html_basics/Text"
+import Button, { ButtonTemplates } from "../../components/html_basics/Button";
 
 const HomePage = () => {
 
+    const GetStartedClickHandler = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        console.log("source -> ", e)
+        console.log("Let's get started")
+    }
+
     return (
-        <div>
+        <div className="homepage">
+            <div className="text-content">
+                <div className="slogan">
+                    <Text text="Cosmic Bonds," template={TextTemplates.HERO} />
+                    <Text text="Stellar Returns." template={TextTemplates.HERO} />
+                </div>
+                <Button disabled={false}
+                    template={ButtonTemplates.FIILED}
+                    text="Get Started"
+                    onClick={e => GetStartedClickHandler(e)} />
+            </div>
             <PlanetView />
         </div>
     )
