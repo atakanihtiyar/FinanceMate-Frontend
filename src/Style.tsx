0b22f8c35d88e9cf026415d1e0e1f86f82776c05
atakan ihtiyar <ataberk97@hotmail.com>
2024-03-25 18:27:53 +0300

    add some global variables

---
+++ src/Style.tsx
@@ -0,0 +1,19 @@
+export enum Sizes {
+    SMALL = "small",
+    REGULAR = "regular",
+    MEDIUM = "medium",
+    LARGE = "large",
+    XLARGE = "xlarge",
+    XXLARGE = "xxlarge"
+}
+
+export enum Colors {
+    WHITE = "white",
+    BLACK = "black",
+    RED = "red"
+}
+
+export enum Fonts {
+    RALEWAY = "raleway",
+    OUTFIT = "outfit"
+}
