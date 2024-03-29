5dcc8a27369cd3178137cf3475374dd7b0b49126
atakan ihtiyar <ataberk97@hotmail.com>
2024-03-29 13:06:31 +0300

    add react router dom package

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import Navbar from './components/navigations/Navbar';
 import Footer from './components/navigations/Footer';
 import HomePage from './pages/HomePage';
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Navbar />
+        <HomePage />
+        <Footer />
+      </>
+    ),
+  },
+]);
+
 function App() {
 
   return (
-    <>
-      <Navbar />
-      <HomePage />
-      <Footer />
-    </>
+    <RouterProvider router={router} />
   );
 }
 
