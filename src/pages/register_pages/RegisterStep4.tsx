31c6031baf5ba21ba81ac55bd1b995f30301c9a6
ati <26187453+atakanihtiyar@users.noreply.github.com>
2024-05-01 19:15:44 +0300

    add step 4 and disclosure section to form

---
+++ src/pages/register_pages/RegisterStep4.tsx
@@ -0,0 +1,183 @@
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {
+    Card,
+    CardContent
+} from "@/components/ui/card"
+
+import { Button } from "@/components/ui/button"
+import { useContext } from "react"
+import { RegisterDataContext } from "./RegisterPage"
+import { Checkbox } from "@/components/ui/checkbox"
+
+interface Props {
+    goPreStep: () => void,
+    goNextStep: () => void
+}
+
+const RegisterStep4 = ({ goPreStep, goNextStep }: Props) => {
+    const { formData, setRegisterData } = useContext(RegisterDataContext)
+
+    const formSchema = z.object({
+        disclosures: z.object({
+            is_control_person: z.boolean(),
+            is_affiliated_exchange_or_finra: z.boolean(),
+            is_politically_exposed: z.boolean(),
+            immediate_family_exposed: z.boolean(),
+        })
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            disclosures: {
+                is_control_person: formData.alpaca.disclosures.is_control_person,
+                is_affiliated_exchange_or_finra: formData.alpaca.disclosures.is_affiliated_exchange_or_finra,
+                is_politically_exposed: formData.alpaca.disclosures.is_politically_exposed,
+                immediate_family_exposed: formData.alpaca.disclosures.immediate_family_exposed,
+            }
+        },
+
+    })
+
+    const handleGoNext = (values: z.infer<typeof formSchema>) => {
+        setRegisterData({
+            is_control_person: values.disclosures.is_control_person,
+            is_affiliated_exchange_or_finra: values.disclosures.is_affiliated_exchange_or_finra,
+            is_politically_exposed: values.disclosures.is_politically_exposed,
+            immediate_family_exposed: values.disclosures.immediate_family_exposed,
+        })
+        goNextStep()
+    }
+
+    const handleGoPre = () => {
+        setRegisterData({
+            is_control_person: form.getValues("disclosures.is_control_person"),
+            is_affiliated_exchange_or_finra: form.getValues("disclosures.is_affiliated_exchange_or_finra"),
+            is_politically_exposed: form.getValues("disclosures.is_politically_exposed"),
+            immediate_family_exposed: form.getValues("disclosures.immediate_family_exposed"),
+        })
+        goPreStep()
+    }
+
+    return (
+        <Card className="tw-w-[400px] tw-min-h-[500px] tw-drop-shadow-[0_0_32px_rgba(238,238,238,0.1)]">
+            <CardContent className="tw-w-full tw-mt-12">
+                <Form {...form}>
+                    <form className="tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-4">
+                        {/* DISCLOSURES */}
+                        <FormField
+                            control={form.control}
+                            name="disclosures"
+                            render={() => (
+                                <FormItem>
+                                    <div className="tw-mb-0">
+                                        <FormLabel>Disclosures</FormLabel>
+                                    </div>
+                                    <div>
+                                        {/* IS CONTROL PERSON */}
+                                        <FormField
+                                            control={form.control}
+                                            name="disclosures.is_control_person"
+                                            render={({ field }) => (
+                                                <FormItem className="tw-w-full tw-flex tw-flex-row tw-justify-start tw-items-center tw-space-x-2 !tw-space-y-0 tw-my-1 tw-py-1">
+                                                    <FormControl>
+                                                        <Checkbox
+                                                            checked={field.value}
+                                                            onCheckedChange={field.onChange}
+                                                        />
+                                                    </FormControl>
+                                                    <div className="tw-space-y-1 tw-leading-none">
+                                                        <FormLabel className="tw-font-normal tw-text-center tw-align-middle">
+                                                            Do you hold a significant leadership role in a publicly traded company?
+                                                        </FormLabel>
+                                                    </div>
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* IS AFFILLIATED EXCHANGE OR FINRA */}
+                                        <FormField
+                                            control={form.control}
+                                            name="disclosures.is_affiliated_exchange_or_finra"
+                                            render={({ field }) => (
+                                                <FormItem className="tw-w-full tw-flex tw-flex-row tw-justify-start tw-items-center tw-space-x-2 !tw-space-y-0 tw-my-1 tw-py-1">
+                                                    <FormControl>
+                                                        <Checkbox
+                                                            checked={field.value}
+                                                            onCheckedChange={field.onChange}
+                                                        />
+                                                    </FormControl>
+                                                    <div className="tw-space-y-1 tw-leading-none">
+                                                        <FormLabel className="tw-font-normal tw-text-center tw-align-middle">
+                                                            Are you affiliated with a stock exchange or FINRA?
+                                                        </FormLabel>
+                                                    </div>
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* IS POLITICALLY EXPOSED */}
+                                        <FormField
+                                            control={form.control}
+                                            name="disclosures.is_politically_exposed"
+                                            render={({ field }) => (
+                                                <FormItem className="tw-w-full tw-flex tw-flex-row tw-justify-start tw-items-center tw-space-x-2 !tw-space-y-0 tw-my-1 tw-py-1">
+                                                    <FormControl>
+                                                        <Checkbox
+                                                            checked={field.value}
+                                                            onCheckedChange={field.onChange}
+                                                        />
+                                                    </FormControl>
+                                                    <div className="tw-space-y-1 tw-leading-none">
+                                                        <FormLabel className="tw-font-normal tw-text-center tw-align-middle">
+                                                            Are you a politically exposed person (PEP)
+                                                        </FormLabel>
+                                                    </div>
+                                                </FormItem>
+                                            )}
+                                        />
+                                        {/* IS POLITICALLY EXPOSED */}
+                                        <FormField
+                                            control={form.control}
+                                            name="disclosures.immediate_family_exposed"
+                                            render={({ field }) => (
+                                                <FormItem className="tw-w-full tw-flex tw-flex-row tw-justify-start tw-items-center tw-space-x-2 !tw-space-y-0 tw-my-1 tw-py-1">
+                                                    <FormControl>
+                                                        <Checkbox
+                                                            checked={field.value}
+                                                            onCheckedChange={field.onChange}
+                                                        />
+                                                    </FormControl>
+                                                    <div className="tw-space-y-1 tw-leading-none">
+                                                        <FormLabel className="tw-font-normal tw-text-center tw-align-middle">
+                                                            Is any of your immediate family a PEP or hold a significant leadership role in a publicly traded company?
+                                                        </FormLabel>
+                                                    </div>
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="tw-flex tw-justify-between tw-mb-4 tw-mt-4 tw-w-[80%]">
+                            <Button type="button" variant={"outline"} className="tw-font-semibold" onClick={handleGoPre} size="sm">Back</Button>
+                            <Button type="submit" className="tw-font-semibold" onClick={form.handleSubmit(handleGoNext)} size="sm">Next</Button>
+                        </div>
+                    </form>
+                </Form>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default RegisterStep4
--- src/pages/register_pages/RegisterSteps.tsx
+++ src/pages/register_pages/RegisterSteps.tsx
@@ -6,12 +6,13 @@ import { useContext, useState } from "react"
 import { RegisterDataContext } from "./RegisterPage"
 import RegisterStep3 from "./RegisterStep3"
 import RegisterStepLast from "./RegisterStepLast"
+import RegisterStep4 from "./RegisterStep4"
 
 export const RegisterSteps = () => {
     const { register } = useContext(RegisterDataContext)
     const [activeStep, setActiveStep] = useState(1)
     const minStep = 1
-    const maxStep = 4
+    const maxStep = 5
 
     const goPreStep = () => {
         setActiveStep(oldStep => oldStep === minStep ? oldStep : --oldStep)
@@ -37,13 +38,13 @@ export const RegisterSteps = () => {
     }
 
     return (
-
         <Tabs defaultValue={"1"} value={activeStep.toString()}>
             <TabsList className="tw-w-[400px] tw-h-[40px] !tw-bg-[--background] tw-border-[1px] tw-border-[--muted] tw-border-solid">
                 <TabsTrigger value="1" onClick={() => goToStep(1)} className={generateTabTriggerClassName(1)}>Base</TabsTrigger>
                 <TabsTrigger value="2" onClick={() => goToStep(2)} className={generateTabTriggerClassName(2)}>Identity</TabsTrigger>
                 <TabsTrigger value="3" onClick={() => goToStep(3)} className={generateTabTriggerClassName(3)}>Contact</TabsTrigger>
-                <TabsTrigger value="4" onClick={() => goToStep(4)} className={generateTabTriggerClassName(4)}>Overview</TabsTrigger>
+                <TabsTrigger value="4" onClick={() => goToStep(4)} className={generateTabTriggerClassName(4)}>Disclosures</TabsTrigger>
+                <TabsTrigger value="5" onClick={() => goToStep(5)} className={generateTabTriggerClassName(5)}>Overview</TabsTrigger>
             </TabsList>
             <TabsContent value="1">
                 <RegisterStep1 goNextStep={goNextStep} />
@@ -55,6 +56,9 @@ export const RegisterSteps = () => {
                 <RegisterStep3 goPreStep={goPreStep} goNextStep={goNextStep} />
             </TabsContent>
             <TabsContent value="4">
+                <RegisterStep4 goPreStep={goPreStep} goNextStep={goNextStep} />
+            </TabsContent>
+            <TabsContent value="5">
                 <RegisterStepLast goPreStep={goPreStep} onSubmit={onSubmit} />
             </TabsContent>
         </Tabs>
