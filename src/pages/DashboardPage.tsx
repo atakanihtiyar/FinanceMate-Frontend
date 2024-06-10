c4da9f5cfd80f236f4c1a0ac3616c8ae8c72647b
ati <26187453+atakanihtiyar@users.noreply.github.com>
2024-06-10 18:33:14 +0300

    update dashboard order list visual

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -14,6 +14,16 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table"
+
+import {
+    Sheet,
+    SheetContent,
+    SheetHeader,
+    SheetTitle,
+} from "@/components/ui/sheet"
+
+import { ScrollArea } from "@/components/ui/scroll-area"
+
 import { useContext, useEffect, useState } from "react"
 import { UserContext, UserContextValues } from "@/context/UserContext"
 import { Separator } from "@/components/ui/separator"
@@ -53,17 +63,28 @@ const DashboardPage = () => {
     const [orders, setOrders] = useState([{
         order_id: "",
         symbol: "",
-        filled_at: "",
+        asset_class: "",
         created_at: "",
+        updated_at: "",
+        submitted_at: "",
+        filled_at: "",
+        expired_at: "",
+        cancelled_at: "",
+        failed_at: "",
+        status: "status",
         qty: "0",
         filled_qty: "0",
         filled_avg_price: "0",
-        order_type: "",
+        type: "",
+        time_in_force: "",
         side: "",
         limit_price: "0",
         stop_price: "0",
         commission: "0",
+        commission_bps: "0",
     }])
+    const [dialogOpen, setDialogOpen] = useState<boolean>(false)
+    const [dialogOrder, setDialogOrder] = useState<typeof orders[0]>(orders[0])
 
     useEffect(() => {
         if (!isAuthRequestEnd) return
@@ -198,35 +219,110 @@ const DashboardPage = () => {
                             <Table>
                                 <TableHeader>
                                     <TableRow>
+                                        <TableHead>Side</TableHead>
                                         <TableHead className="w-[100px]">Symbol</TableHead>
-                                        <TableHead>Created At</TableHead>
-                                        <TableHead>Filled At</TableHead>
-                                        <TableHead>Qty</TableHead>
-                                        <TableHead>Limit</TableHead>
-                                        <TableHead>Stop</TableHead>
-                                        <TableHead className="text-right">Side</TableHead>
+                                        <TableHead>Filled Qty</TableHead>
+                                        <TableHead>Status</TableHead>
+                                        <TableHead className="text-right">Created At</TableHead>
                                     </TableRow>
                                 </TableHeader>
                                 <TableBody>
-                                    {
-                                        orders.length > 0 ?
-                                            orders.map((item) => {
-                                                return (
-                                                    <TableRow key={item.order_id}>
-                                                        <TableCell className="font-medium">{item.symbol}</TableCell>
-                                                        <TableCell>{new Date(item.created_at).toLocaleString()}</TableCell>
-                                                        <TableCell>{item.filled_at ? (new Date(item.filled_at).toLocaleString()) : "-"}</TableCell>
-                                                        <TableCell>{item.filled_at ? item.filled_qty : item.qty}</TableCell>
-                                                        <TableCell>{item.limit_price ? item.limit_price : "-"}</TableCell>
-                                                        <TableCell>{item.stop_price ? item.stop_price : "-"}</TableCell>
-                                                        <TableCell className="text-right">{item.side}</TableCell>
-                                                    </TableRow>)
-                                            }) : (
-                                                <TableRow>
-                                                    <TableCell className="text-center" colSpan={7}>No Data</TableCell>
-                                                </TableRow>
-                                            )
-                                    }
+                                    <Sheet open={dialogOpen} onOpenChange={setDialogOpen} >
+                                        {
+                                            orders.length > 0 ?
+                                                orders.map((item) => {
+                                                    return (
+                                                        <TableRow key={item.order_id} className="cursor-pointer hover:bg-accent" onClick={() => {
+                                                            setDialogOpen(true)
+                                                            setDialogOrder(item)
+                                                        }}>
+                                                            <TableCell>
+                                                                {item.side === "buy" ? "Buy" : item.side === "sell" ? "Sell" : "-"}
+                                                            </TableCell>
+                                                            <TableCell>{item.symbol}</TableCell>
+                                                            <TableCell>{item.filled_qty}</TableCell>
+                                                            <TableCell>{item.status[0].toUpperCase() + item.status.slice(1)}</TableCell>
+                                                            <TableCell className="text-right">{new Date(item.created_at).toLocaleString()}</TableCell>
+                                                        </TableRow>
+                                                    )
+                                                }) : (
+                                                    <TableRow>
+                                                        <TableCell className="text-center" colSpan={7}>No Data</TableCell>
+                                                    </TableRow>
+                                                )
+                                        }
+                                        <SheetContent className={`min-w-max`}>
+                                            <SheetHeader>
+                                                <SheetTitle>Order Details</SheetTitle>
+                                            </SheetHeader>
+                                            <ScrollArea className={`min-h-max w-[512px] my-4 p-4`}>
+                                                <div className="flex flex-col">
+                                                    <div className="w-full grid grid-cols-12">
+                                                        <div className="col-start-1 col-span-4 space-y-2">
+                                                            <p className="text-muted-foreground">Order ID</p>
+                                                        </div>
+                                                        <div className="col-start-5 col-span-8 space-y-2">
+                                                            <p>{dialogOrder.order_id ? dialogOrder.order_id : "-"}</p>
+                                                        </div>
+                                                    </div>
+                                                    <Separator className="my-4" />
+                                                    <div className="w-full grid grid-cols-12">
+                                                        <div className="col-start-1 col-span-4 space-y-2">
+                                                            <p className="text-muted-foreground">Symbol</p>
+                                                            <p className="text-muted-foreground">Asset Class</p>
+                                                            <p className="text-muted-foreground">Quantity</p>
+                                                            <p className="text-muted-foreground">Filled Quantity</p>
+                                                            <p className="text-muted-foreground">Filled Average Price</p>
+                                                            <p className="text-muted-foreground">Commission</p>
+                                                            <p className="text-muted-foreground">Commission BPS</p>
+                                                            <p className="text-muted-foreground">Type</p>
+                                                            <p className="text-muted-foreground">Side</p>
+                                                            <p className="text-muted-foreground">Time In Force</p>
+                                                            <p className="text-muted-foreground">Limit Price</p>
+                                                            <p className="text-muted-foreground">Stop Price</p>
+                                                            <p className="text-muted-foreground">Status</p>
+                                                        </div>
+                                                        <div className="col-start-5 col-span-8 space-y-2">
+                                                            <p>{dialogOrder.symbol ? dialogOrder.symbol : "-"}</p>
+                                                            <p>{dialogOrder.asset_class ? dialogOrder.asset_class : "-"}</p>
+                                                            <p>{dialogOrder.qty ? dialogOrder.qty : "-"}</p>
+                                                            <p>{dialogOrder.filled_qty ? dialogOrder.filled_qty : "-"}</p>
+                                                            <p>{dialogOrder.filled_avg_price ? "$ " + dialogOrder.filled_avg_price : "-"}</p>
+                                                            <p>{dialogOrder.commission ? dialogOrder.commission : "-"}</p>
+                                                            <p>{dialogOrder.commission_bps ? dialogOrder.commission_bps : "-"}</p>
+                                                            <p>{dialogOrder.type ? dialogOrder.type : "-"}</p>
+                                                            <p>{dialogOrder.side ? dialogOrder.side : "-"}</p>
+                                                            <p>{dialogOrder.time_in_force ? dialogOrder.time_in_force.toUpperCase() : "-"}</p>
+                                                            <p>{dialogOrder.limit_price ? "$ " + dialogOrder.limit_price : "-"}</p>
+                                                            <p>{dialogOrder.stop_price ? "$ " + dialogOrder.stop_price : "-"}</p>
+                                                            <p>{dialogOrder.status ? dialogOrder.status : "-"}</p>
+                                                        </div>
+                                                    </div>
+                                                    <Separator className="my-4" />
+                                                    <div className="w-full grid grid-cols-12">
+                                                        <div className="col-start-1 col-span-4 space-y-2">
+                                                            <p className="text-muted-foreground">Created At</p>
+                                                            <p className="text-muted-foreground">Updated At</p>
+                                                            <p className="text-muted-foreground">Submitted At</p>
+                                                            <p className="text-muted-foreground">Filled At</p>
+                                                            <p className="text-muted-foreground">Expired At</p>
+                                                            <p className="text-muted-foreground">Cancelled At</p>
+                                                            <p className="text-muted-foreground">Failed At</p>
+                                                        </div>
+                                                        <div className="col-start-5 col-span-8 space-y-2">
+                                                            <p>{dialogOrder.created_at ? new Date(dialogOrder.created_at).toLocaleString() : "-"}</p>
+                                                            <p>{dialogOrder.updated_at ? new Date(dialogOrder.updated_at).toLocaleString() : "-"}</p>
+                                                            <p>{dialogOrder.submitted_at ? new Date(dialogOrder.submitted_at).toLocaleString() : "-"}</p>
+                                                            <p>{dialogOrder.filled_at ? new Date(dialogOrder.filled_at).toLocaleString() : "-"}</p>
+                                                            <p>{dialogOrder.expired_at ? new Date(dialogOrder.expired_at).toLocaleString() : "-"}</p>
+                                                            <p>{dialogOrder.cancelled_at ? new Date(dialogOrder.cancelled_at).toLocaleString() : "-"}</p>
+                                                            <p>{dialogOrder.failed_at ? new Date(dialogOrder.failed_at).toLocaleString() : "-"}</p>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </ScrollArea>
+                                        </SheetContent>
+                                    </Sheet>
                                 </TableBody>
                             </Table>
                         </CardContent>
